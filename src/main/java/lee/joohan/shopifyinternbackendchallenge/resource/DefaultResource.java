9d3dbf6d4b4786f2f9174f46f317743c814be31c
Joohan Lee <joohan33@gmail.com>
2022-05-22 22:40:09 -0400

    Added entry page

---
+++ src/main/java/lee/joohan/shopifyinternbackendchallenge/resource/DefaultResource.java
@@ -0,0 +1,21 @@
+package lee.joohan.shopifyinternbackendchallenge.resource;
+
+import lee.joohan.shopifyinternbackendchallenge.dto.inventory.response.GetInventoryItemsResponse;
+import lee.joohan.shopifyinternbackendchallenge.model.InventoryItem;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class DefaultResource {
+    @GetMapping
+    public ResponseEntity entry() {
+        return ResponseEntity.ok("Hello world!");
+    }
+
+}
